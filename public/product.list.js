68ddd186a5f9dfd776c535ad5292944c04de3b1b
virgilhawkins00 <munizalex45@gmail.com>
2023-08-20 18:55:45 -0300

    product and user lists

---
+++ public/product.list.js
@@ -0,0 +1,225 @@
+const PRODUCT_LIST = [
+    {
+        name: 'Deviate NITRO Men\'s Running Shoes',
+        image: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/194449/08/sv01/fnd/PNA/fmt/png/Deviate-NITRO-Men's-Running-Shoes",
+        image1: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/194449/08/fnd/PNA/fmt/png/Deviate-NITRO-Men's-Running-Shoes",
+        image2: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/194449/08/sv02/fnd/PNA/fmt/png/Deviate-NITRO-Men's-Running-Shoes",
+        image3: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/194449/08/sv04/fnd/PNA/fmt/png/Deviate-NITRO-Men's-Running-Shoes",
+        brand: 'Puma',
+        category: 'Running',
+        description: 'Beat your time then beat it again in Deviate NITRO. This high-performance running shoe is packed with tech for the fastest runs. At the bottom, you get durable, PUMAGRIP traction and a carbon fiber INNOPLATE. This innovative plate acts as a lever for maximum energy transfer at toe-off. For the long runs that outlast the daylight, you get 360 reflectivity and lightweight NITRO foam, giving you max cushioning with improved efficiency. Deviate NITRO is a breathable, form-fitting shoe for an effortless run.',
+        rating: '4.8',
+        reviews: [
+            {
+                name: '',
+                rating: '',
+                comment: '',
+            }
+        ],
+        numOfReviews: '37',
+        price: '160',
+        salePrice: '160',
+        countInStock: '20',
+    },
+    {
+        name: 'Liberate NITRO Men\'s Running Shoes',
+        image: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/194917/01/sv01/fnd/PNA/fmt/png/Liberate-NITRO-Men's-Running-Shoes",
+        image1: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/194917/01/fnd/PNA/fmt/png/Liberate-NITRO-Men's-Running-Shoes",
+        image2: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/194917/01/sv04/fnd/PNA/fmt/png/Liberate-NITRO-Men's-Running-Shoes",
+        image3: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/194917/01/bv/fnd/PNA/fmt/png/Liberate-NITRO-Men's-Running-Shoes",
+        brand: 'Puma',
+        category: 'Running',
+        description: 'Liberate NITRO keeps you light on your feet with an effortless ride. Form fit around PUMA\'s new performance last, Liberate NITRO offers a great fit executed in a sturdy mono- mesh.A one - piece, NITRO midsole delivers just - right cushioning with great responsiveness for tempo runs and competition.To keep you steady and seen, Liberate NITRO has a TPU heel piece providing proprioception, and strategically placed PUMAGRIP for all - surface traction.',
+        rating: '4.1',
+        reviews: [
+            {
+                name: '',
+                rating: '',
+                comment: '',
+            }
+        ],
+        numOfReviews: '10',
+        price: '59.99',
+        salePrice: '110',
+        countInStock: '20',
+    },
+    {
+        name: 'evoSPEED Tokyo Future JUMP Track and Field Shoes',
+        image: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/376381/01/sv01/fnd/PNA/fmt/png/evoSPEED-Tokyo-Future-JUMP-Track-and-Field-Shoes",
+        image1: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/376381/01/bv/fnd/PNA/fmt/png/evoSPEED-Tokyo-Future-JUMP-Track-and-Field-Shoes",
+        image2: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/376381/01/sv02/fnd/PNA/fmt/png/evoSPEED-Tokyo-Future-JUMP-Track-and-Field-Shoes",
+        image3: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/376381/01/sv04/fnd/PNA/fmt/png/evoSPEED-Tokyo-Future-JUMP-Track-and-Field-Shoes",
+        brand: 'Puma',
+        category: 'Running',
+        description: 'In need of maximum speed on the runway? Looking for the strongest possible takeoff? These track and field shoes, approved by World Athletics, are the answer for any pole vaulter or long jumper looking to up their game. There\'s an internal carbon fibre plate from the midfoot to the forefoot to give you ultimate propulsion.Plus, there\'s PUMA\'s Matryx Micro technology in the upper, which provides additional support for jumping.With lightweight grip, these shoes will help carry you higher and further than ever before.',
+        rating: '0',
+        reviews: [
+            {
+                name: '',
+                rating: '',
+                comment: '',
+            }
+        ],
+        numOfReviews: '0',
+        price: '64.99',
+        salePrice: '90',
+        countInStock: '20',
+    },
+    {
+        name: 'Suede Classic XXI Men\'s Sneakers',
+        image: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/374915/30/sv01/fnd/PNA/fmt/png/Suede-Classic-XXI-Men's-Sneakers",
+        image1: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/374915/30/fnd/PNA/fmt/png/Suede-Classic-XXI-Men's-Sneakers",
+        image2: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/374915/30/bv/fnd/PNA/fmt/png/Suede-Classic-XXI-Men's-Sneakers",
+        image3: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/374915/30/sv04/fnd/PNA/fmt/png/Suede-Classic-XXI-Men's-Sneakers",
+        brand: 'Puma',
+        category: 'Sneakers',
+        description: 'The Suede hit the scene in 1968 and has been changing the game ever since. It’s been worn by the icons of every generation and it’s stayed classic through it all. This year, we relaunch the Suede with fresh colorways and subtle design updates. Classic as ever, for all-time.',
+        rating: '4',
+        reviews: [
+            {
+                name: '',
+                rating: '',
+                comment: '',
+            }
+        ],
+        numOfReviews: '66',
+        price: '69.99',
+        salePrice: '69.99',
+        countInStock: '20',
+    },
+    {
+        name: 'Speedcat LS Men\'s Motorsport Shoes',
+        image: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/380173/10/sv01/fnd/PNA/fmt/png/Speedcat-LS-Men's-Motorsport-Shoes",
+        image1: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/380173/10/fnd/PNA/fmt/png/Speedcat-LS-Men's-Motorsport-Shoes",
+        image2: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/380173/10/bv/fnd/PNA/fmt/png/Speedcat-LS-Men's-Motorsport-Shoes",
+        image3: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/380173/10/sv04/fnd/PNA/fmt/png/Speedcat-LS-Men's-Motorsport-Shoes",
+        brand: 'Puma',
+        category: 'Sneakers',
+        description: 'You weren’t made to follow, so don’t. Deviate from the pack in Speedcat. This classic kick has owned the intersection between track and street for generations– and it’s showing no signs of slowing down. Wrapped in rich suede and finished off with a tire tread bottom, it’s a little unorthodox– but then again, so are you. So take a chance, stand out, and dare to differ. But whatever you do, do it in Speedcat',
+        rating: '4',
+        reviews: [
+            {
+                name: '',
+                rating: '',
+                comment: '',
+            }
+        ],
+        numOfReviews: '29',
+        price: '90',
+        salePrice: '90',
+        countInStock: '20',
+    },
+    {
+        name: 'Nike Air Zoom Pegasus 38',
+        image: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/19ace622-7e4a-4427-b606-79cc1aa1418e/air-zoom-pegasus-38-road-running-shoes-22XRPM.png",
+        image1: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/01bfd32c-2f81-4563-89bf-f66bfd1424b8/air-zoom-pegasus-38-road-running-shoes-22XRPM.png",
+        image2: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/b289b473-d847-4ac3-8628-276ad98208af/air-zoom-pegasus-38-road-running-shoes-22XRPM.png",
+        image3: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/9893b0c2-c8a9-494e-bf9c-16bfb2d67736/air-zoom-pegasus-38-road-running-shoes-22XRPM.png",
+        brand: 'Nike',
+        category: 'Running',
+        description: 'Bring some smiles to your miles as you run the streets of Tokyo.The Pegasus 38 is a tried-and-true classic that keeps you bouncy and comfortable, bringing a little extra joy to your longer runs.Bold, geometric shapes celebrate the cityscape as you stride into the distance.Something Old, Something New. A wider forefoot means more space for your toes, while the shoe\'s fit maintains the comfortable feel you expect from the Pegasus.The upper is breathable and plush while stretching with your foot for a comfortable shape and fit. Spring with Your Step. Nike React foam is lightweight, springy and durable.More foam means better cushioning without the bulk.A Zoom Air unit puts more bounce with every step.It\'s closer to your foot for responsiveness. Secure Fit. Midfoot webbing gives a snug fit when you tighten the laces.',
+        rating: '0',
+        reviews: [
+            {
+                name: '',
+                rating: '',
+                comment: '',
+            }
+        ],
+        numOfReviews: '0',
+        price: '170',
+        salePrice: '170',
+        countInStock: '20',
+    },
+    {
+        name: 'Nike Air Zoom Terra Kiger 7',
+        image: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/74d643af-8231-4576-ae67-50432da73516/air-zoom-terra-kiger-7-trail-running-shoes-PFRMNz.png",
+        image1: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/e3caa80f-87d2-4247-877f-9b6820eea07e/air-zoom-terra-kiger-7-trail-running-shoes-PFRMNz.png",
+        image2: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/6cab379f-280d-4296-9307-05bbab986677/air-zoom-terra-kiger-7-trail-running-shoes-PFRMNz.png",
+        image3: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/e54559fe-a549-43fa-add7-27e9b1b64fc7/air-zoom-terra-kiger-7-trail-running-shoes-PFRMNz.png",
+        brand: 'Nike',
+        category: 'Running',
+        description: 'Run the trail in a super-responsive running shoe.Fast and lightweight, it delivers a breathable and secure feel as you race over rocky paths.Updated traction lugs provide stability for your downhill miles. Push Forwards. Nike React technology is a lightweight and durable foam that delivers a responsive ride.A Zoom Air unit in the forefoot provides extra responsiveness, returning your energy to help guide you down the trail. Multi - Surface Traction. Multi- directional traction lugs in the heel are made from high - abrasion rubber.They help provide enhanced grip on descents and ascents. Light and Breathable. A mesh and synthetic upper delivers a lightweight feel and enhanced ventilation. Trail - Ready Support. A Dynamic Fit System through the midfoot helps hug and support your foot as you run through rocky trails.',
+        rating: '5',
+        reviews: [
+            {
+                name: '',
+                rating: '',
+                comment: '',
+            }
+        ],
+        numOfReviews: '43',
+        price: '180',
+        salePrice: '180',
+        countInStock: '20',
+    },
+    {
+        name: 'Nike Air VaporMax 2021 FK',
+        image: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/5d241ea3-c87b-4902-84f1-250c508e980d/air-vapormax-2021-fk-shoes-DgPVmr.png",
+        image1: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/2283a4f6-882a-4e61-a66d-08c42f1306d0/air-vapormax-2021-fk-shoes-DgPVmr.png",
+        image2: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/1d5cd50b-31a3-4fb5-bc28-86d36a968938/air-vapormax-2021-fk-shoes-DgPVmr.png",
+        image3: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/2ce2217c-41f2-40a6-aa39-6b7e8e411ac4/air-vapormax-2021-fk-shoes-DgPVmr.png",
+        brand: 'Nike',
+        category: 'Sneakers',
+        description: 'Made from at least 40% recycled materials by weight, the Nike Air VaporMax 2021 FK is airy and easy to wear with super-stretchy, recycled Flyknit fabric, plus a soft collar that sculpts your ankle.The stitched-on Swoosh and recycled TPU heel clip add a splash of intrigue as you float down the streets on incredibly soft VaporMax cushioning. Run the trail in a super-responsive running shoe.Fast and lightweight, it delivers a breathable and secure feel as you race over rocky paths.Updated traction lugs provide stability for your downhill miles.',
+        rating: '0',
+        reviews: [
+            {
+                name: '',
+                rating: '',
+                comment: '',
+            }
+        ],
+        numOfReviews: '0',
+        price: '290',
+        salePrice: '260',
+        countInStock: '20',
+    },
+    {
+        name: 'Nike Air Max Plus III',
+        image: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/1cbf2075-7f55-4303-a0b4-b54360fa18f9/air-max-plus-iii-shoe-MRt6Ct.png",
+        image1: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/e393da49-ffe3-433c-93dc-f829333e5b93/air-max-plus-iii-shoe-MRt6Ct.png",
+        image2: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/16eb06cf-512f-411f-90fa-c0e681fe56d2/air-max-plus-iii-shoe-MRt6Ct.png",
+        image3: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/16eb06cf-512f-411f-90fa-c0e681fe56d2/air-max-plus-iii-shoe-MRt6Ct.png",
+        brand: 'Nike',
+        category: 'Sneakers',
+        description: 'Featuring the same Tuned Air technology as its predecessors, the Nike Air Max Plus III updates the look with plastic accents fused to the upper while paying homage to the iconic colour fade of the OG. Run the trail in a super-responsive running shoe.Fast and lightweight, it delivers a breathable and secure feel as you race over rocky paths.Updated traction lugs provide stability for your downhill miles.',
+        rating: '4.5',
+        reviews: [
+            {
+                name: '',
+                rating: '',
+                comment: '',
+            }
+        ],
+        numOfReviews: '6',
+        price: '280',
+        salePrice: '235',
+        countInStock: '20',
+    },
+    {
+        name: 'ULTRABOOST DNA X LEGO® COLORS SHOES',
+        image: "https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/6a0d6246e26a4852825dad3900baddfd_9366/Ultraboost_DNA_x_LEGO(r)_Colors_Shoes_White_FZ3983_01_standard.jpg",
+        image1: "https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/dfd4c55131254e48a481ad3900baec2f_9366/Ultraboost_DNA_x_LEGO(r)_Colors_Shoes_White_FZ3983_02_standard.jpg",
+        image2: "https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/9878e6718b5145b9bfbdad3900bafad9_9366/Ultraboost_DNA_x_LEGO(r)_Colors_Shoes_White_FZ3983_04_standard.jpg",
+        image3: "https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/33025f283c4646809d12ad3900bb247b_9366/Ultraboost_DNA_x_LEGO(r)_Colors_Shoes_White_FZ3983_09_standard.jpg",
+        brand: 'Adidas',
+        category: 'Running',
+        description: 'HIGH-PERFORMANCE RUNNING SHOES MADE IN PARTNERSHIP WITH THE LEGO GROUP.Running is your time to play.And if you couldn\'t tell by the pops of color and LEGO® bricks inspired design, these adidas running shoes created with the LEGO Group are all about play. Play, and comfort. Because nothing needs to get in the way of a good time. A plush Boost midsole takes care of the cushioning, and the Continental™ Better Rubber outsole balances fast moves with steady grounding.',
+        rating: '4.9',
+        reviews: [
+            {
+                name: '',
+                rating: '',
+                comment: '',
+            }
+        ],
+        numOfReviews: '312',
+        price: '250',
+        salePrice: '250',
+        countInStock: '20',
+    },
+
+]
+
+export default PRODUCT_LIST;
---
+++ public/user.list.js
@@ -0,0 +1,9 @@
+const USERS = [
+    {
+        name: 'Admin',
+        email: 'admin@admintest.com',
+        isAdmin: true,
+    }
+]
+
+export default USERS
